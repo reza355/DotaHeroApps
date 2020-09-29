cb49993a14f2c67756853a1c760c6cabca07838a
Fathureza Januarza <fathureza.januarza@tokopedia.com>
2020-09-29 16:03:10 +0700

    setup collection view

---
+++ DotaHeroApps/HeroLists/Model/HeroRealmModel.swift
@@ -0,0 +1,9 @@
+//
+//  HeroRealmModel.swift
+//  DotaHeroApps
+//
+//  Created by Fathureza Januarza on 29/09/20.
+//  Copyright © 2020 Fathureza Januarza. All rights reserved.
+//
+
+import Foundation
